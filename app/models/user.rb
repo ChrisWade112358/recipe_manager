7b097bf2d25120de6d6fefbd7636da9f6a6acf6a
swade781977 <swade781977@yahoo.com>
2021-01-06 02:41:25 -0500

    added model method for omniauth and made omniauth controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+
+  def self.create_from_provider_date(data)
+    where(provider: data.provider, uid: data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = provider_data.info.name
+    end
+  end
 end
