e471207d71aa982ab1cde35bf963145ffedbe883
swade781977 <swade781977@yahoo.com>
2021-01-06 02:47:12 -0500

    added registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :recipies
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
