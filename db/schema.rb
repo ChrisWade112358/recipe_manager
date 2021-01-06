2bd6910b926455eb0aee4e222169372a8ef323bc
swade781977 <swade781977@yahoo.com>
2021-01-06 03:51:04 -0500

    added recipe_id to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_084204) do
+ActiveRecord::Schema.define(version: 2021_01_06_084956) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_084204) do
     t.datetime "expiration_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "recipe_id", null: false
   end
 
   create_table "recipies", force: :cascade do |t|
